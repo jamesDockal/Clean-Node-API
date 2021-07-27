bdb9edfa7890bd569165b09f9edbdd19d798d900
james dockal <jamesistranho@gmail.com>
2021-07-26 21:10:56 -0300

    feat: ensure SingUpController return 400 if no name is provided

---
+++ src/presentation/controllers/signup.ts
@@ -0,0 +1,7 @@
+export default class SingUpController {
+  handle (httpRequest: any): any {
+    return {
+      statusCode: 400
+    }
+  }
+}
---
+++ src/presentation/controllers/singup.test.ts
@@ -0,0 +1,17 @@
+import SingUpController from './signup'
+
+describe('SingUp controller', () => {
+  it('should return 400 if no name was provided', () => {
+    const sut = new SingUpController()
+    const httpRequest = {
+      body: {
+        email: 'any_email',
+        password: 'any_password',
+        passwordConfirmation: 'any_passwordConfirmation'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
