90cf8a644b6d3482a87d7d8d12f3afab2247bfb3
J. Dockal <james.dockal@xa.epicgames.com>
2022-10-24 16:23:11 -0300

    refactor: move all protocols imports to a single file

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,11 @@
 import { InvalidParamError, MissingParamError } from '../erros';
+import {
+	Controller,
+	EmailValidator,
+	HttpRequest,
+	HttpResponse,
+} from '../protocols';
 import { badRequest, serverError } from '../helpers/http-helper';
-import { Controller } from '../protocols/controller';
-import { EmailValidator } from '../protocols/email-validator';
-import { HttpRequest, HttpResponse } from '../protocols/http';
 
 export class SignUpController implements Controller {
 	private readonly emailValidator: EmailValidator;
