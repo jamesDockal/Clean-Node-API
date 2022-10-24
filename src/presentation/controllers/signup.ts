96bb73c8075af506b0885b0619271ab16cd9a282
J. Dockal <james.dockal@xa.epicgames.com>
2022-10-24 16:39:43 -0300

    refactor: remove repeated code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -22,17 +22,20 @@ export class SignUpController implements Controller {
 				'password',
 				'passwordConfirmation',
 			];
+			const { body } = httpRequest;
+
 			for (const field of requiredFields) {
-				if (!httpRequest.body[field]) {
+				if (!body[field]) {
 					return badRequest(new MissingParamError(field));
 				}
 			}
+			const { email, password, passwordConfirmation } = body;
 
-			if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+			if (password !== passwordConfirmation) {
 				return badRequest(new InvalidParamError('passwordConfirmation'));
 			}
 
-			const isValid = this.emailValidator.isValid(httpRequest.body.email);
+			const isValid = this.emailValidator.isValid(email);
 			if (!isValid) {
 				return badRequest(new InvalidParamError('email'));
 			}
