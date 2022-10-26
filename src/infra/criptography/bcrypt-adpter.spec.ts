5c06f77508fc2ca2a1b3e023ad8e31541508a60a
J. Dockal <james.dockal@xa.epicgames.com>
2022-10-25 21:29:10 -0300

    test: ensure BcryptAdapter returns a hash on success

--- src/infra/criptography/bcrypt-adpter.spec.ts
+++ src/infra/criptography/bcrypt-adpter.spec.ts
@@ -1,6 +1,12 @@
 import bcrypt from 'bcrypt';
 import { BCryptAdapter } from './bcrypt-adapter';
 
+jest.mock('bcrypt', () => ({
+	async hash(): Promise<string> {
+		return await new Promise((resolve) => resolve('hashed_value'));
+	},
+}));
+
 describe('Bcrypt Adapter', () => {
 	test('should call bcrypt with correct values', async () => {
 		const salt = 12;
@@ -10,5 +16,14 @@ describe('Bcrypt Adapter', () => {
 		const value = 'any_value';
 		await sut.encrypt(value);
 		expect(hashSpy).toHaveBeenCalledWith(value, salt);
+	});
+
+	test('should return a hash on success', async () => {
+		const salt = 12;
+		const sut = new BCryptAdapter(salt);
+
+		const hashedValue = await sut.encrypt('any_value');
+
+		expect(hashedValue).toBe('hashed_value');
 	});
 });
