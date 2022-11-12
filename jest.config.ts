396b3779e2d7906dea8e435b182b7a660bcab550
J. Dockal <james.dockal@xa.epicgames.com>
2022-11-12 11:07:49 -0300

    feat: ensure mongodb reconnect if client is down

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,7 @@
 export default {
 	roots: ['<rootDir>/src'],
 	collectCoverage: true,
-	collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+	collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
 	clearMocks: true,
 	coverageDirectory: 'coverage',
 	coverageProvider: 'v8',
