5c3c48b66fd9ec54b4438722288b63f3e7962901
J. Dockal <james.dockal@xa.epicgames.com>
2022-11-12 14:23:26 -0300

    feat: ensure LogControllerDecorator calls controller with correct values

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,8 +3,10 @@ import { DbAddAccount } from '../../data/useCases/add-account/db-add-account';
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter';
 import { BCryptAdapter } from '../../infra/criptography/bcrypt-adapter';
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account';
+import { LogControllerDecorator } from '../decorators/log';
+import { Controller } from '../../presentation/protocols';
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
 	const salt = 12;
 	const bcryptAdapter = new BCryptAdapter(salt);
 	const accountMongoRepository = new AccountMongoRepository();
@@ -13,5 +15,10 @@ export const makeSignUpController = (): SignUpController => {
 
 	const emailValidatorAdapter = new EmailValidatorAdapter();
 
-	return new SignUpController(emailValidatorAdapter, dbAddAccount);
+	const signUpController = new SignUpController(
+		emailValidatorAdapter,
+		dbAddAccount
+	);
+
+	return new LogControllerDecorator(signUpController);
 };
