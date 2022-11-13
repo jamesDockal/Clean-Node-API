35e88b1bbf06e26c45238013db13aef9273b8744
J. Dockal <james.dockal@xa.epicgames.com>
2022-11-13 17:44:22 -0300

    feat: ensure LoginController calls Authentication with correct values

--- src/presentation/controllers/Login/login.ts
+++ src/presentation/controllers/Login/login.ts
@@ -1,13 +1,15 @@
 import { InvalidParamError, MissingParamError } from '../../erros';
 import { badRequest, serverError } from '../../helpers/http-helper';
 import { Controller, HttpRequest, HttpResponse } from '../../protocols';
-import { EmailValidator } from './login-protocols';
+import { EmailValidator, Authentication } from './login-protocols';
 
 export class LoginController implements Controller {
 	private readonly emailValidator: EmailValidator;
+	private readonly authentication: Authentication;
 
-	constructor(emailValidator: EmailValidator) {
+	constructor(emailValidator: EmailValidator, authentication: Authentication) {
 		this.emailValidator = emailValidator;
+		this.authentication = authentication;
 	}
 
 	async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
@@ -20,12 +22,14 @@ export class LoginController implements Controller {
 					return badRequest(new MissingParamError(field));
 				}
 			}
-			const { email } = body;
+			const { email, password } = body;
 
 			const isValid = this.emailValidator.isValid(email);
 			if (!isValid) {
 				return badRequest(new InvalidParamError('email'));
 			}
+
+			await this.authentication.auth(email, password);
 		} catch (error) {
 			return serverError(error);
 		}
