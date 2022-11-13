99fa1d60207881e9ac01e824a00216a85e011407
J. Dockal <james.dockal@xa.epicgames.com>
2022-11-13 15:17:43 -0300

    feat: ensure LogControllerDecorator calls LogErrorRepository if controller returns a server error

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,3 +1,5 @@
+import { LogErrorRepository } from '../../data/protocols/log-error-repository';
+import { serverError } from '../../presentation/helpers/http-helper';
 import {
 	Controller,
 	HttpRequest,
@@ -5,9 +7,20 @@ import {
 } from '../../presentation/protocols';
 import { LogControllerDecorator } from './log';
 
+const makeLogErrorRepository = (): LogErrorRepository => {
+	class LogErrorRepositoryStub implements LogErrorRepository {
+		async log(stackError: string): Promise<void> {
+			return await new Promise((resolve) => resolve(null));
+		}
+	}
+
+	return new LogErrorRepositoryStub();
+};
+
 interface SutTypes {
 	sut: LogControllerDecorator;
 	controllerStub: Controller;
+	logErrorRepositoryStub: LogErrorRepository;
 }
 
 const makeSut = (): SutTypes => {
@@ -23,11 +36,17 @@ const makeSut = (): SutTypes => {
 	}
 
 	const controllerStub = new ControllerStub();
-	const sut = new LogControllerDecorator(controllerStub);
+	const logErrorRepositoryStub = makeLogErrorRepository();
+
+	const sut = new LogControllerDecorator(
+		controllerStub,
+		logErrorRepositoryStub
+	);
 
 	return {
 		sut,
 		controllerStub,
+		logErrorRepositoryStub,
 	};
 };
 
@@ -48,5 +67,31 @@ describe('Log Controller Decorator', () => {
 		await sut.handle(httpRequest);
 
 		expect(controllerStubHandleSpy).toBeCalledWith(httpRequest);
+	});
+
+	test('should call LogErrorRepository with correct error if controller returns a server error', async () => {
+		const { sut, controllerStub, logErrorRepositoryStub } = makeSut();
+		const fakeError = new Error();
+		fakeError.stack = 'any_stack';
+
+		const error = serverError(fakeError);
+		const logSpy = jest.spyOn(logErrorRepositoryStub, 'log');
+
+		jest
+			.spyOn(controllerStub, 'handle')
+			.mockReturnValueOnce(new Promise((resolve) => resolve(error)));
+
+		const httpRequest = {
+			body: {
+				name: 'any_name',
+				email: 'any_email@mail.com',
+				password: 'any_password',
+				passwordConfirmation: 'any_password',
+			},
+		};
+
+		await sut.handle(httpRequest);
+
+		expect(logSpy).toHaveBeenCalledWith('any_stack');
 	});
 });
