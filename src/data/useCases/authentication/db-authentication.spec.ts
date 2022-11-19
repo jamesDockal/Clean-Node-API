d29dbb98d6ae96b7066944f3ae2e0bc3795662e6
J. Dockal <james.dockal@xa.epicgames.com>
2022-11-19 16:10:47 -0300

    feat: ensure DbAuthentication returns a token on success

--- src/data/useCases/authentication/db-authentication.spec.ts
+++ src/data/useCases/authentication/db-authentication.spec.ts
@@ -1,10 +1,11 @@
 import { HashComparer } from '../../protocols/criptography/hash-comparer';
+import { TokenGenerator } from '../../protocols/criptography/token-generator';
 import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository';
 import { AccountModel } from '../add-account/db-add-account-protocols';
 import { DbAuthentication } from './db-authentication';
 
 const makeFakeAccount = (): AccountModel => ({
-	id: 'any_',
+	id: 'any_id',
 	email: 'any_email@mail.com',
 	name: 'any_name',
 	password: 'hash_password',
@@ -39,24 +40,37 @@ const makeHashComparer = (): HashComparer => {
 	return new HashComparerStub();
 };
 
+const makeTokenGenerator = (): TokenGenerator => {
+	class TokenGeneratorStub implements TokenGenerator {
+		async generate(value: string): Promise<string> {
+			return 'access_token';
+		}
+	}
+	return new TokenGeneratorStub();
+};
+
 interface SutTypes {
 	sut: DbAuthentication;
 	loadAccountByEmailRepositoryStub: LoadAccountByEmailRepository;
 	hashComparerStub: HashComparer;
+	tokenGeneratorStub: TokenGenerator;
 }
 
 const makeSut = (): SutTypes => {
 	const loadAccountByEmailRepositoryStub = makeLoadAccountByEmailRepository();
 	const hashComparerStub = makeHashComparer();
+	const tokenGeneratorStub = makeTokenGenerator();
 	const sut = new DbAuthentication(
 		loadAccountByEmailRepositoryStub,
-		hashComparerStub
+		hashComparerStub,
+		tokenGeneratorStub
 	);
 
 	return {
 		sut,
 		loadAccountByEmailRepositoryStub,
 		hashComparerStub,
+		tokenGeneratorStub,
 	};
 };
 
@@ -99,5 +113,33 @@ describe('DbAuthentication UseCase', () => {
 		await sut.auth(makeFakeAuthentication());
 
 		expect(hashComparerSpy).toBeCalledWith('any_password', 'hash_password');
+	});
+
+	test('should return null if HashComparer returns false', async () => {
+		const { sut, hashComparerStub } = makeSut();
+		jest
+			.spyOn(hashComparerStub, 'compare')
+			.mockReturnValueOnce(new Promise((resolve) => resolve(false)));
+
+		const accessToken = await sut.auth(makeFakeAuthentication());
+
+		expect(accessToken).toBeNull();
+	});
+
+	test('should call TokenGenerator with correct id', async () => {
+		const { sut, tokenGeneratorStub } = makeSut();
+		const tokenSpy = jest.spyOn(tokenGeneratorStub, 'generate');
+
+		await sut.auth(makeFakeAuthentication());
+
+		expect(tokenSpy).toBeCalledWith('any_id');
+	});
+
+	test('should return access token', async () => {
+		const { sut } = makeSut();
+
+		const accessToken = await sut.auth(makeFakeAuthentication());
+
+		expect(accessToken).toBe('access_token');
 	});
 });
--- src/data/useCases/authentication/db-authentication.ts
+++ src/data/useCases/authentication/db-authentication.ts
@@ -3,12 +3,14 @@ import {
 	AuthenticationModel,
 } from '../../../domain/useCases/authentication';
 import { HashComparer } from '../../protocols/criptography/hash-comparer';
+import { TokenGenerator } from '../../protocols/criptography/token-generator';
 import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository';
 
 export class DbAuthentication implements Authentication {
 	constructor(
 		private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
-		private readonly hashComparer: HashComparer
+		private readonly hashComparer: HashComparer,
+		private readonly tokenGenerator: TokenGenerator
 	) {}
 
 	async auth(authenticationModel: AuthenticationModel): Promise<string> {
@@ -16,10 +18,14 @@ export class DbAuthentication implements Authentication {
 			authenticationModel.email
 		);
 		if (account) {
-			await this.hashComparer.compare(
+			const isValid = await this.hashComparer.compare(
 				authenticationModel.password,
 				account.password
 			);
+
+			if (isValid) {
+				return await this.tokenGenerator.generate(account.id);
+			}
 		}
 
 		return null;
