d43560b1c504aaa29ac5d87df7046d84fca66a5d
J. Dockal <james.dockal@xa.epicgames.com>
2022-11-21 12:03:04 -0300

    refactor: move sut creation to a factory helper method

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -7,9 +7,21 @@ jest.mock('jsonwebtoken', () => ({
 	},
 }));
 
+interface SutTypes {
+	sut: JwtAdapter;
+}
+
+const makeSut = (): SutTypes => {
+	const sut = new JwtAdapter('secret');
+
+	return {
+		sut,
+	};
+};
+
 describe('Jwt Adapter', () => {
 	test('should call sign with correct values', async () => {
-		const sut = new JwtAdapter('secret');
+		const { sut } = makeSut();
 		const signSpy = jest.spyOn(jwt, 'sign');
 		await sut.generate('any_id');
 		expect(signSpy).toHaveBeenCalledWith(
@@ -21,7 +33,7 @@ describe('Jwt Adapter', () => {
 	});
 
 	test('should return a token on sign success', async () => {
-		const sut = new JwtAdapter('secret');
+		const { sut } = makeSut();
 		const token = await sut.generate('any_id');
 		expect(token).toBe('any_token');
 	});
