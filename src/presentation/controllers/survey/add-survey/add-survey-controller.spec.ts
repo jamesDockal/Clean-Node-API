7bdfa02ffd516d10a890ed6b27968ddba09daef0
Dockal <jamesdockal@gmail.com>
2022-12-07 11:42:26 -0300

    feat: ensure AddSurveyController calls AddSurveyUseCase with correct values

--- src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.spec.ts
@@ -1,3 +1,4 @@
+import { AddSurvey, AddSurveyModel } from 'domain/useCases/add-survey';
 import { Validation } from 'presentation/helpers/validators/validation';
 import { badRequest } from '../../../../presentation/helpers/http-helper';
 import { AddSurveyController } from './add-survey-controller';
@@ -25,19 +26,32 @@ const makeValidation = (): Validation => {
 	return new ValidationStub();
 };
 
+const makeAddSurvey = (): AddSurvey => {
+	class AddSurveyStub implements AddSurvey {
+		async add(data: AddSurveyModel): Promise<void> {
+			return await new Promise((resolve) => resolve());
+		}
+	}
+
+	return new AddSurveyStub();
+};
+
 interface SutTypes {
 	sut: AddSurveyController;
 	validationStub: Validation;
+	addSurveyStub: AddSurvey;
 }
 
 const makeSut = (): SutTypes => {
 	const validationStub = makeValidation();
+	const addSurveyStub = makeAddSurvey();
 
-	const sut = new AddSurveyController(validationStub);
+	const sut = new AddSurveyController(validationStub, addSurveyStub);
 
 	return {
 		sut,
 		validationStub,
+		addSurveyStub,
 	};
 };
 
@@ -56,5 +70,13 @@ describe('AddSurvey Controller', () => {
 		const httpRequest = makeFakeRequest();
 		const response = await sut.handle(httpRequest);
 		expect(response).toEqual(badRequest(new Error()));
+	});
+
+	test('should call AddSurvey with correct values', async () => {
+		const { sut, addSurveyStub } = makeSut();
+		const addSurveySpy = jest.spyOn(addSurveyStub, 'add');
+		const httpRequest = makeFakeRequest();
+		await sut.handle(httpRequest);
+		expect(addSurveySpy).toHaveBeenCalledWith(httpRequest.body);
 	});
 });
--- src/presentation/controllers/survey/add-survey/add-survey-controller.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controller.ts
@@ -1,15 +1,21 @@
 import { badRequest } from '../../../../presentation/helpers/http-helper';
 import { Validation } from 'presentation/helpers/validators/validation';
 import { Controller, HttpRequest, HttpResponse } from './add-survey-protocols';
+import { AddSurvey } from 'domain/useCases/add-survey';
 
 export class AddSurveyController implements Controller {
-	constructor(private readonly validation: Validation) {}
+	constructor(
+		private readonly validation: Validation,
+		private readonly addSurvey: AddSurvey
+	) {}
 
 	async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
 		const error = this.validation.validate(httpRequest.body);
 		if (error) {
 			return badRequest(error);
 		}
+
+		await this.addSurvey.add(httpRequest.body);
 
 		return null;
 	}
