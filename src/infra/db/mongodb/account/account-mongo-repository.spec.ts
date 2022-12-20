d611ff10081ea77f72a144ecfb30fd3ed304750e
Dockal <jamesdockal@gmail.com>
2022-12-20 17:30:41 -0300

    feat: ensure AccountMongoRepository find an account by token with role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -83,7 +83,7 @@ describe('Account Mongo Repository', () => {
 	});
 
 	describe('loadByToken()', () => {
-		test('should return an account on loadByToken success withoud role', async () => {
+		test('should return an account on loadByToken success without role', async () => {
 			const sut = new AccountMongoRepository();
 
 			await accountCollection.insertOne({
@@ -94,6 +94,28 @@ describe('Account Mongo Repository', () => {
 			});
 
 			const account = await sut.loadByToken('any_token');
+
+			expect(account).toBeTruthy();
+
+			expect(account).toBeTruthy();
+			expect(account.id).toBeTruthy();
+			expect(account.name).toBe('any_name');
+			expect(account.email).toBe('any_email@mail.com');
+			expect(account.password).toBe('any_password');
+		});
+
+		test('should return an account on loadByToken success with role', async () => {
+			const sut = new AccountMongoRepository();
+
+			await accountCollection.insertOne({
+				name: 'any_name',
+				email: 'any_email@mail.com',
+				password: 'any_password',
+				accessToken: 'any_token',
+				role: 'any_role',
+			});
+
+			const account = await sut.loadByToken('any_token', 'any_role');
 
 			expect(account).toBeTruthy();
 
