c47ea2609f8ab1fbb6faa1bbede8b90c501e0f75
Dockal <jamesdockal@gmail.com>
2022-12-20 17:11:01 -0300

    feat: ensure JwtAdapter call verify with correct values

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -5,6 +5,9 @@ jest.mock('jsonwebtoken', () => ({
 	async sign(): Promise<string> {
 		return 'any_token';
 	},
+	async verify(): Promise<string> {
+		return 'any_token';
+	},
 }));
 
 interface SutTypes {
@@ -20,21 +23,33 @@ const makeSut = (): SutTypes => {
 };
 
 describe('Jwt Adapter', () => {
-	test('should call sign with correct values', async () => {
-		const { sut } = makeSut();
-		const signSpy = jest.spyOn(jwt, 'sign');
-		await sut.generate('any_id');
-		expect(signSpy).toHaveBeenCalledWith(
-			{
-				id: 'any_id',
-			},
-			'secret'
-		);
+	describe('sign()', () => {
+		test('should call sign with correct values', async () => {
+			const { sut } = makeSut();
+			const signSpy = jest.spyOn(jwt, 'sign');
+			await sut.generate('any_id');
+			expect(signSpy).toHaveBeenCalledWith(
+				{
+					id: 'any_id',
+				},
+				'secret'
+			);
+		});
+
+		test('should return a token on sign success', async () => {
+			const { sut } = makeSut();
+			const token = await sut.generate('any_id');
+			expect(token).toBe('any_token');
+		});
 	});
 
-	test('should return a token on sign success', async () => {
-		const { sut } = makeSut();
-		const token = await sut.generate('any_id');
-		expect(token).toBe('any_token');
+	describe('verify()', () => {
+		test('should call verify with correct values', async () => {
+			const { sut } = makeSut();
+
+			const verifySpy = jest.spyOn(jwt, 'verify');
+			await sut.decrypt('any_token');
+			expect(verifySpy).toBeCalledWith('any_token', 'secret');
+		});
 	});
 });
--- src/infra/criptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.ts
@@ -1,11 +1,18 @@
+import { Decrypter } from 'data/protocols/criptography/decrypter';
 import jwt from 'jsonwebtoken';
 
 import { TokenGenerator } from '../../../data/protocols/criptography/token-generator';
 
-export class JwtAdapter implements TokenGenerator {
+export class JwtAdapter implements TokenGenerator, Decrypter {
 	constructor(private readonly secret: string) {}
 
 	async generate(value: string): Promise<string> {
 		return jwt.sign({ id: value }, this.secret);
+	}
+
+	async decrypt(value: string): Promise<string> {
+		await jwt.verify(value, this.secret);
+
+		return null;
 	}
 }
